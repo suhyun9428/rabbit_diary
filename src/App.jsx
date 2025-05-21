87edec45eb3bd2d035dda6f724ea2ed5b2b038c2
김수현 <hvl1789@gepplus.co.kr>
2025-05-21 09:08:07 +0900

    mockData 추가

--- src/App.jsx
+++ src/App.jsx
@@ -81,20 +81,37 @@ const App = () => {
 
   useEffect(() => {
     const storedData = localStorage.getItem('diary');
-    if(!storedData){
+    if (!storedData) {
+      const mockData = [
+        {
+          id : 1,
+          createdDate : new Date('2025-02-28').getTime(),
+          emotionId : 1,
+          content : '1번 일기 오늘은 2월 28일 아이 추워~ 1번 일기 오늘은 2월 28일 아이 추워~  1번 일기 오늘은 2월 28일 아이 추워~ 1번 일기 오늘은 2월 28일 아이 추워~ 1번 일기 오늘은 2월 28일 아이 추워~'
+        },
+        {
+          id : 2,
+          createdDate : new Date('2025-03-15').getTime(),
+          emotionId : 2,
+          content : '2번 일기 오늘은 3월 15일~'
+        }
+      ];
+      localStorage.setItem('diary', JSON.stringify(mockData));
+      dispatch({ type: 'INIT', data: mockData });
+      idRef.current = 3;
       setIsLoading(false);
       return;
     }
-    const parsedData = JSON.parse(storedData);
 
-    if(!Array.isArray(parsedData)){
+    const parsedData = JSON.parse(storedData);
+    if (!Array.isArray(parsedData)) {
       setIsLoading(false);
-      return;
-    };
+    return;
+    }
 
     let maxId = 0;
     parsedData.forEach((item) => {
-      if(Number(item.id) > maxId ){
+      if (Number(item.id) > maxId) {
         maxId = Number(item.id);
       }
     });
@@ -102,12 +119,13 @@ const App = () => {
     idRef.current = maxId + 1;
 
     dispatch({
-      type : 'INIT',
-      data : parsedData,
+      type: 'INIT',
+      data: parsedData,
     });
 
     setIsLoading(false);
   }, []);
+
 
   const onCreate = (createdDate, emotionId, content) => {
     dispatch({
